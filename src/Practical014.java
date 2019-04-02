866ea81d9d41261e51f18cf5fbdd34e93b9a25b7
jay-suthar <jaysuthar67@gmail.com>
2019-04-02 16:03:31 +0530

    Jay

--- src/Practical014.java
+++ src/Practical014.java
@@ -1,2 +1,40 @@
+import jdk.nashorn.api.tree.CatchTree;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
+
+
 public class Practical014 {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost/demostud";
+        String Uname = "root";
+        String pass = "";
+        String query = "SELECT * FROM STUDENT_MASTER";
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Enroll no :");
+        scn.nextInt();
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Trying To connect...");
+            Connection con = DriverManager.getConnection(url, Uname, pass);
+            System.out.println("Connected Successfully");
+            Statement st = con.createStatement();
+            System.out.println("Statement Created Successfully");
+            ResultSet rs = st.executeQuery(query);
+            System.out.println("Query Execuated Successfully");
+            rs.close();
+            st.close();
+            con.close();
+        } catch (SQLException sqlex) {
+            sqlex.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
