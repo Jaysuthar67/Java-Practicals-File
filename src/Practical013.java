5b9dce39df8025813f642690786b7af0bab9999d
jay-suthar <jaysuthar67@gmail.com>
2019-04-02 15:51:27 +0530

    Jay

--- src/Practical013.java
+++ src/Practical013.java
@@ -15,7 +15,11 @@ public class Practical013 {
             System.out.println("Statement Created Successfully");
             ResultSet rs = st.executeQuery(query);
             System.out.println("Query Execuated Successfully");
-            System.out.println(rs);
+            System.out.println("| Enroll | Name | City |");
+            System.out.println("|----------------------|");
+            while (rs.next()) {
+                System.out.println("| " + rs.getInt("enroll"));
+            }
         } catch (SQLException sqlex) {
             sqlex.printStackTrace();
         } catch (Exception e) {
